828fefb99b438329e2cb06402121923b528b6454
Jakub Jachowicz <jakub.jachowicz@student.pk.edu.pl>
2024-12-21 16:51:25 +0100

    czas jest az zbyt wzgledny

---
+++ kompilacja.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Kompilacja programu kasjer
+gcc -o kasjer kasjer.c
+if [ $? -ne 0 ]; then
+    echo "Błąd kompilacji kasjer.c"
+    exit 1
+fi
+
+# Kompilacja programu klient
+gcc -o klient klient.c
+if [ $? -ne 0 ]; then
+    echo "Błąd kompilacji klient.c"
+    exit 1
+fi
+
+# Kompilacja programu symulacja z użyciem pthread
+gcc -pthread -o symulacja symulacja.c
+if [ $? -ne 0 ]; then
+    echo "Błąd kompilacji symulacja.c"
+    exit 1
+fi
+
+echo "Kompilacja zakończona pomyślnie."
